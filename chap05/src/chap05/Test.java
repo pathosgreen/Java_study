7a7505fddb7a7cd02921fd264e4ede3eeb587d03
pathosgreen <hexun_@naver.com>
2022-05-10 18:03:15 +0900

    2022_05_10

---
+++ chap05/src/chap05/Test.java
@@ -0,0 +1,24 @@
+package chap05;
+
+public class Test implements Sam {
+	
+	
+	@Override
+	public void aaa() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bbb(int num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ccc() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
